30bfcd2992ac853dc6b038a600452af3290821eb
Jake Hamill <hamilljake1@gmail.com>
2016-11-01 09:05:43 -0500

    as;jkdh

--- sketch.js
+++ sketch.js
@@ -15,15 +15,17 @@ function setup() {
    var y = random(height);
    blue[i] = new Cellshuffle(cell, x, y);
  }
+
 }
 
 function draw() {
   background(255);
   background(220, 0, 0, 100);
 
-  for (var i = 0; i < blue.length; i++)
-   blue[i].move();
-   blue[i].display();
+  for (var i = 0; i < blue.length; i++) {
+    blue[i].move();
+    blue[i].display();
+  }
 }
 
 function Cellshuffle(img, tempX, tempY) {
@@ -32,14 +34,13 @@ function Cellshuffle(img, tempX, tempY) {
   this.y = tempY;
   this.cellImage = img;
 
-this.move = function() {
+  this.move = function() {
+    this.x += sin(HALF_PI) * 5;
+    this.y += random(-200, 800);
+  }
 
-  this.x += sin(HALF_PI) * 5;
-  this.y += random(-200, 800);
-
-}
-this.display = function() {
-  image(this.cellImage, this.x, this.y);
-
- }
+  this.display = function() {
+    image(this.cellImage, this.x, this.y);
+  }
+  
 }
